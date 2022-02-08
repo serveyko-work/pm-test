6ca331f4cb49d6e70267e0105d19b9c0c19c081e
Sergey Solovey <maximum.iphone.mi@gmail.com>
2022-02-08 23:37:18 +0300

    add redux thunk

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,17 @@
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-const store = createStore( rootReducer );
-export default store;
+import { applyMiddleware, compose, createStore } from 'redux';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+
+export const composeEnhancers = typeof window === 'object' && process.env.REACT_APP_DEVTOOLS === 'true' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+export const store = createStore(
+  rootReducer,
+  {},
+  composeEnhancers(
+    applyMiddleware(
+      thunk,
+    )
+  ),
+);
+
 export type RootState = ReturnType<typeof store.getState>
